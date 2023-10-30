ae1317caf2f66ff4531a9e0c1b5d1dd52a9452bb
Marius Topircean <mtopircean@yahoo.com>
2023-10-30 17:45:57 +0000

    Install summernote

--- website/admin.py
+++ website/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 from .models import Ailment
 from .models import AddProduct
 from .models import AddPromotion
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(AddProduct)
+class AddProductAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
+
+
+@admin.register(AddPromotion)
+class AddPromotionAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('description')
+
 
 admin.site.register(Ailment)
-admin.site.register(AddProduct)
-admin.site.register(AddPromotion)
