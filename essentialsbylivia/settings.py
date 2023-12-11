201af61c0e43aadd10b72166f042a9fd3da6e15f
Marius Topircean <mtopircean@yahoo.com>
2023-12-11 19:59:43 +0000

    Update debug setting in settings

--- essentialsbylivia/settings.py
+++ essentialsbylivia/settings.py
@@ -22,7 +22,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEBUG'
+DEBUG = 'DEBUG' in os.environ
 ALLOWED_HOSTS = ['8000-mtopircean-essentialsby-d45xfzluakj.ws-eu106.gitpod.io','8000-mtopircean-essentialsby-d45xfzluakj.ws-eu105.gitpod.io', 'essentials-by-livia-efe89c429260.herokuapp.com', 'django-blog-tmf-e46d0a2b5195.herokuapp.com', 'localhost']
 
 
