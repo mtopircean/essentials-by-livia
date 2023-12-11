c9f71c84fe46d509e2c1b65d4b7e901e0c7d7c24
Marius Topircean <mtopircean@yahoo.com>
2023-12-11 06:28:12 +0000

    Update introduction section in readme

--- essentialsbylivia/settings.py
+++ essentialsbylivia/settings.py
@@ -22,7 +22,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-mtopircean-essentialsby-d45xfzluakj.ws-eu105.gitpod.io', 'essentials-by-livia-efe89c429260.herokuapp.com', 'django-blog-tmf-e46d0a2b5195.herokuapp.com', 'localhost']
 
