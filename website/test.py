9823dc4c2339f1497515b73a83476559bd8c39e7
Marius Topircean <mtopircean@yahoo.com>
2023-12-12 21:59:38 +0000

    Update test.py imports and test cases

--- website/test.py
+++ website/test.py
@@ -1,13 +1,17 @@
+import unittest
+from . import views 
 from django.test import TestCase, Client
 from django.urls import reverse
-from .models import AddPromotion, AddProduct, AppUser, Ailment, FavouriteSelection
 from django.contrib.auth.models import User
+from .models import AddProduct, AddPromotion
 
 class TestViews(TestCase):
+
     def setUp(self):
         self.client = Client()
         self.user = User.objects.create_user(username='testuser', password='testpassword')
-        self.client.login(username='testuser', password='testpassword')
+        self.product = AddProduct.objects.create(name='Test Product', description='Test Description')
+        self.promotion = AddPromotion.objects.create(name='Test Promotion', description='Test Description')
         
     def test_index_page(self):
         response = self.client.get(reverse('index_page'))
